1e85a98f6cde93f886324d74e12888af4d3dcde1
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2022-07-01 19:36:08 +0530

    Added solution - LeetHub

---
+++ Find a pair with given target in BST - GFG/find-a-pair-with-given-target-in-bst.cpp
@@ -0,0 +1,158 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+#define MAX_SIZE 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+ // } Driver Code Ends
+/*Complete the function below
+Node is as follows
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // root : the root Node of the given BST
+    // target : the target sum
+    
+    void inorderTrav(Node* root,vector<int> &v)
+    {
+        if(root!=NULL)
+        {
+            inorderTrav(root->left,v);   
+            v.push_back(root->data);
+            inorderTrav(root->right,v);   
+        }
+    }
+    
+    int isPairPresent(struct Node *root, int sum)
+    {
+        int i,j;
+        vector<int> v;
+        inorderTrav(root,v);
+        
+        int l,r;
+        l = 0;
+        r = v.size()- 1;
+        
+        while (l < r) 
+        {
+            if (v[l] + v[r] == sum)
+                return 1;
+            else if (v[l] + v[r] < sum)
+                l++;
+            else // A[i] + A[j] > sum
+                r--;
+        }
+        return 0;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root = buildTree(s);
+
+       getline(cin, s);
+       int k = stoi(s);
+        //getline(cin, s);
+       Solution obj;    
+       cout << obj.isPairPresent(root, k) << endl;
+       //cout<<"~"<<endl;
+   }
+   return 0;
+}
+  // } Driver Code Ends
