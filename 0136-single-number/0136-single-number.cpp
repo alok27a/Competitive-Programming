b9aae41242f371de458ca9c3075f47e1c5fe298a
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2024-02-05 19:39:00 +0100

    Time: 20 ms (34.02%), Space: 23.6 MB (5.31%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        
+        for(auto i : mp)
+        {
+            if(i.second==1)
+                return i.first;
+        }
+        return 1;
+    }
+};
