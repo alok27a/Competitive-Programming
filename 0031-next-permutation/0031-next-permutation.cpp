eb09016d22b4fc0ec63528f178841aeb9c8f6f61
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2024-11-27 19:08:44 -0500

    Time: 0 ms (100.00%), Space: 15.6 MB (5.58%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& array) {
-        next_permutation(array.begin(), array.end());
+        // next_permutation(array.begin(), array.end());
+        int ind = -1;
+        int n = array.size();
+        
+//         Finding the breakpoint
+        for(int i=n-2;i>=0;i--)
+        {
+            if(array[i] < array[i+1])
+            {
+                ind = i;
+                break;
+            }
+        }
+        if(ind==-1)
+        {
+            reverse(array.begin(),array.end());
+            return;
+        }
+        
+//      Swapping ind with the minimum of the remaining
+        
+        for(int i=n-1;i>ind;i--)
+        {
+            if(array[i]>array[ind])
+            {
+                swap(array[i],array[ind]);
+                break;
+            }
+        }
+        
+        reverse(array.begin()+ind+1,array.end());
     }
 };
