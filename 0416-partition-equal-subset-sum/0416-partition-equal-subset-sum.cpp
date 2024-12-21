36daac5323f32f48dd07d9ef45ad347b81cc3393
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2024-12-21 08:30:54 +0530

    Time: 135 ms (60.94%), Space: 100.2 MB (27.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int memo[201][10001];
     
-    bool dp(vector<int>& nums, int i, int l, int val) {
-        if (i >= l) return val == 0;
-        if (val < 0) return false;
-        if (memo[i][val] != -1) return memo[i][val];
-        bool res = dp(nums, i + 1, l, val - nums[i]);
-        res = res || dp(nums, i + 1, l, val);
-        memo[i][val] = res;
-        return memo[i][val];
-    }
+    bool solve(vector<int>& nums,int ind, int target, vector<vector<int>> &dp)
+    {
+        if(target==0) return true;
+        if(ind==0) return (nums[0]==target);
+        
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        
+        bool exc = solve(nums,ind-1,target,dp);
+        bool inc = false;
+        if(nums[ind]<=target) inc = solve(nums,ind-1,target-nums[ind],dp);
+        
+        return dp[ind][target] = exc || inc;
+    }    
     
     bool canPartition(vector<int>& nums) {
-        memset(memo, -1, sizeof(memo));
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % 2) return false;
-        return dp(nums, 0, nums.size(), sum >> 1);
+        int arr_sum = 0;
+        for(auto i:nums) arr_sum+=i;
+        
+        int target = arr_sum/2;
+        
+        if(arr_sum%2!=0) return false;
+        
+        vector<vector<int>> dp(nums.size(), vector<int>(target+1,-1));
+        
+        return solve(nums,nums.size()-1,target,dp);
     }
 };
