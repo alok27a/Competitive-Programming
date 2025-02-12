0d32f1df5b3997939a794ac732e4af29e2b96da2
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2025-02-12 10:12:18 -0500

    Time: 3 ms (89.96%), Space: 23.8 MB (58.46%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        priority_queue<pair<int,int>, 
-                        vector<pair<int,int>>,
-                        greater<pair<int,int>>> pq;
-    
-        for(int i=0;i<intervals.size();i++)
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans; 
+
+        for(int i=0;i<n;i++)
         {
-            pq.push({intervals[i][0],intervals[i][1]});
-        }
-        
-        int s = pq.top().first;
-        int e = pq.top().second;
-        pq.pop();
-        vector<pair<int,int>> res;
-        
-        while(!pq.empty())
-        {
-            auto i = pq.top();
-            int cs = i.first;
-            int ce = i.second;
-            pq.pop();
-            
-            if(e>=cs)
-            {
-                e = max(e,ce);
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
             }
-            else
-            {
-                res.push_back({s,e});
-                s = cs;
-                e = ce;
-            }            
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
         }
-        res.push_back({s,e});
-        vector<vector<int>> ans;
-        
-        for(auto i : res)
-        {
-            int a = i.first;
-            int b = i.second;
-            ans.push_back({a,b});
-        }
+
         return ans;
     }
 };
